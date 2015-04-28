6a051518156b6d3ebffc3b4ce9afc39b5f0c7fec
hrhm2 <kiyasume415@gmail.com>
2015-04-28 16:16:22 +0900

    RandomWalker

---
+++ mySketch/RandomWalkre/src/RandomWalker.h
@@ -0,0 +1,20 @@
+//
+//  RandomWalker.h
+//  RandomWalkre
+//
+//  Created by labメンバー on 2015/04/28.
+//
+//
+
+#pragma once
+#include "ofMain.h"
+
+class RandomWalker {
+public:
+    RandomWalker();
+    void update();
+    void draw();
+    
+    ofVec2f position;
+    
+};
